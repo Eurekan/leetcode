9fd607d9b72f89a20d14f7fa9fa882ffa062cb9f
Eurekan <2455096251@qq.com>
2025-01-20 23:13:52 +0800

    2025/1/20 链表 移除元素

---
+++ src/main/java/algorithm/linkedList/RemoveElements.java
@@ -0,0 +1,29 @@
+package algorithm.linkedList;
+
+/**
+ * 203.移除元素
+ */
+public class RemoveElements {
+    /**
+     * 虚拟头节点
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(1)
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+}
